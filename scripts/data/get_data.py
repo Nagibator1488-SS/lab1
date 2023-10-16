da2d2dbda38bcc53e97d0b7c7c50e1b587bddfc4
Nagibator1488-SS <72550914+Nagibator1488-SS@users.noreply.github.com>
2023-10-16 15:19:37 +0500

    adult

---
+++ scripts/data/get_data.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from catboost.datasets import adult
+train_df, test_df = adult()
+train_df.to_csv("/home/data-srv-admin/lab1/data/raw/train.csv", columns=train.columns)
+test_df.to_csv("/home/data-srv-admin/lab1/data/raw/test.csv", columns=test.columns)
